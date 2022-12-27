15f84fbde4a43e7691f2e34acc8188d790a4a8a2
Storozhuk Denis <Storozhuk.Denis@gmail.com>
2022-12-27 11:59:30 +0300

    control of the last number added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,19 @@
 fun main(args: Array<String>) {
     println("сколько лайков?")
-val likes = readln()
-    if (likes.toInt()==0){
-        println("У вас пока "+likes+" лайков")
-    }else if (likes.toInt()==1){
-        println("У вас "+likes+" лайк")
-    }else if (likes.toInt()>=2&&likes.toInt()<=4){
-        println("У вас "+likes+" лайка")
-    }else if (likes.toInt()>=5){
-        println("У вас "+likes+" лайков")
+    val likes = readln()
+    val likesInt = likes.toInt()
+    val percent = likesInt % 10
+    if (percent == 0) {
+        println("У вас пока " + likes + " лайков")
+    } else if (likesInt >= 2 && likesInt <= 4) {
+        println("У вас " + likes + " лайка")
+    }else if (likesInt==11 || likesInt==111 || likesInt==211|| likesInt==311|| likesInt==411){
+        println("У вас " + likes + " лайков")//Я не придумал ничего лучше:(
+    } else if (percent == 1) {
+        println("У вас " + likes + " лайк")
+    } else if (percent >= 2 && percent <= 4) {
+        println("У вас " + likes + " лайков")
+    } else if (percent >= 5) {
+        println("У вас " + likes + " лайков")
     }
 }
